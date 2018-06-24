2feeac13b0d945971d2cbaab48dbe5b9ec15b334
Caleb Rizo <kaleb.rizzo@gmail.com>
2018-06-24 11:00:47 -0500

    Adds axios and adds axis plot

--- react-plots/src/App.js
+++ react-plots/src/App.js
@@ -1,42 +1,101 @@
 import React, { Component } from 'react'
 import Plot from 'react-plotly.js'
+import axios from 'axios'
 
 import logo from './logo.svg'
 import './App.css'
 import DATA from './plot_mock_data'
 
-const trace = {
-  x: DATA.x,
-  y: DATA.y,
-  legendgroup: 'opennesstoexperience',
-  marker: {color: 'rgb(31, 119, 180)'},
-  mode: 'lines',
-  name: 'opennesstoexperience',
-  showlegend: false,
-  type: 'scatter',
-  xaxis: 'x1',
-  yaxis: 'y1'
-};
-const data = [trace];
-const layout = {
-  barmode: 'overlay',
-  hovermode: 'closest',
-  legend: {traceorder: 'reversed'},
-  title: 'Distplot with Normal Distribution',
-  xaxis1: {
-    anchor: 'y1',
-    domain: [0.0, 1.0],
-    zeroline: false
-  },
-  yaxis1: {
-    anchor: 'x1',
-    domain: [0.35, 1],
-    position: 0.0
-  },
-};
-
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      step: 0,
+      xAxis: [],
+      yAxis: [],
+      zAxis: [],
+      t: [],
+    }
+  }
+
+  componentWillMount() {
+    this._fetchNe2miData()
+  }
+
+  _fetchNe2miData() {
+    axios.get('https://08oio458y8.execute-api.us-east-1.amazonaws.com/dev')
+    .then(resp  => {
+      const { x, y, z, time } = resp.data
+      const plus2 = x => x + 2
+      const xAxis = x.map(plus2)
+      const yAxis = y.map(plus2)
+      const zAxis = z.map(plus2)
+      const length = x.length
+      const t = [...Array(length).keys()].map(x => x * time)
+
+      console.log({xAxis})
+
+      this.setState({ xAxis, yAxis, zAxis, t })
+    })
+  }
+
   render() {
+    const layout = {
+      barmode: 'overlay',
+      hovermode: 'closest',
+      legend: {traceorder: 'reversed'},
+      title: 'Distplot with Normal Distribution',
+      xaxis1: {
+        anchor: 'y1',
+        domain: [0.0, 1.0],
+        zeroline: false
+      },
+      yaxis1: {
+        anchor: 'x1',
+        domain: [0.1, 3],
+        position: 0.0
+      },
+    };
+    const traceX = {
+      x: this.state.t,
+      y: this.state.xAxis,
+      legendgroup: 'opennesstoexperience',
+      marker: {color: 'rgb(31, 119, 180)'},
+      mode: 'lines',
+      name: 'opennesstoexperience',
+      showlegend: false,
+      type: 'scatter',
+      xaxis: 'x1',
+      yaxis: 'y1'
+    };
+    const dataX = [traceX];
+    const traceY = {
+      x: this.state.t,
+      y: this.state.yAxis,
+      legendgroup: 'opennesstoexperience',
+      marker: {color: 'rgb(31, 119, 180)'},
+      mode: 'lines',
+      name: 'opennesstoexperience',
+      showlegend: false,
+      type: 'scatter',
+      xaxis: 'x1',
+      yaxis: 'y1'
+    };
+    const dataY = [traceY];
+    const traceZ = {
+      x: this.state.t,
+      y: this.state.zAxis,
+      legendgroup: 'opennesstoexperience',
+      marker: {color: 'rgb(31, 119, 180)'},
+      mode: 'lines',
+      name: 'opennesstoexperience',
+      showlegend: false,
+      type: 'scatter',
+      xaxis: 'x1',
+      yaxis: 'y1'
+    };
+    const dataZ = [traceZ];
     return (
       <div className="App">
         <header className="App-header">
@@ -44,7 +103,15 @@ class App extends Component {
           <h1 className="App-title">Welcome to Ne<sup>2</sup>mi!</h1>
         </header>
         <Plot
-          data={data}
+          data={dataX}
+          layout={layout}
+        />
+        <Plot
+          data={dataY}
+          layout={layout}
+        />
+        <Plot
+          data={dataZ}
           layout={layout}
         />
       </div>
