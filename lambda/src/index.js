e6f0869d4c36e96afc7a596364593b3addeac23a
Rodrigo Quezada <roderik.ikana@gmail.com>
2018-06-24 13:01:54 -0500

    feat: add algorithmia

--- lambda/src/index.js
+++ lambda/src/index.js
@@ -1,59 +1,80 @@
 const AWS = require('aws-sdk')
 const uuid = require('uuid/v1');
+var algorithmia = require("algorithmia");
+var client = algorithmia(process.env.ALGO_KEY);
 
 AWS.config.update({region:'us-east-1'});
 const dynamodb = new AWS.DynamoDB();
 
-exports.handler = (event, context, callback) => {
-    console.log(event.requestContext.httpMethod)
-    if (event.requestContext.httpMethod === 'POST') {
-        const hist = {
-            Item: {
-                "gait": {
-                    S: uuid()
-                }, 
-                "data": {
-                    S: event.body
-                }
-            }, 
-            ReturnConsumedCapacity: "TOTAL", 
-            TableName: "ne2mi"
-        };
-        const last = {
-            Item: {
-                "gait": {
-                    S: 'last'
-                }, 
-                "data": {
-                    S: event.body
-                }
-            }, 
-            ReturnConsumedCapacity: "TOTAL", 
-            TableName: "ne2mi"
-        };
-        const putItem = (params) => new Promise((resolve,reject) => {
-            dynamodb.putItem(params, function(err, data) {
-                if (err) {
-                    console.log(err, err.stack);
-
-                    reject(err.message)
-                } // an error occurred
-                else {
-                    resolve()
-                }
-            });
+const fourier = (arr) => new Promise((resolve, reject) => {
+    var input = [arr,1,1];
+    client
+        .algo("TimeSeries/FourierFilter/0.1.1")
+        .pipe(input)
+        .then(function(response) {
+            console.log(response.get());
+            resolve(response.get());
         });
+});
 
-        Promise.all([putItem(hist), putItem(last)]).then(() => {
-            callback(null, {
-                "statusCode": "200",
-                "body": "{\"message\":\"ok\"}"
+exports.handler = (event, context, callback) => {
+    if (event.requestContext.httpMethod === 'POST') {
+        const parsedData = JSON.parse(event.body);
+
+        Promise.all([fourier(parsedData.x), fourier(parsedData.y), fourier(parsedData.z)]).then(([x, y , z]) => {
+
+            const newBody = JSON.stringify({
+                x, y ,z , time: parsedData.time
+            })
+
+            const hist = {
+                Item: {
+                    "gait": {
+                        S: uuid()
+                    }, 
+                    "data": {
+                        S: newBody
+                    }
+                }, 
+                ReturnConsumedCapacity: "TOTAL", 
+                TableName: "ne2mi"
+            };
+            const last = {
+                Item: {
+                    "gait": {
+                        S: 'last'
+                    }, 
+                    "data": {
+                        S: newBody
+                    }
+                }, 
+                ReturnConsumedCapacity: "TOTAL", 
+                TableName: "ne2mi"
+            };
+            const putItem = (params) => new Promise((resolve,reject) => {
+                dynamodb.putItem(params, function(err, data) {
+                    if (err) {
+                        console.log(err, err.stack);
+    
+                        reject(err.message)
+                    } // an error occurred
+                    else {
+                        resolve()
+                    }
+                });
             });
-        }).catch((err) => {
-            callback(null, {
-                "statusCode": "500",
-                "body": err
-            });
+    
+            Promise.all([putItem(hist), putItem(last)]).then(() => {
+                callback(null, {
+                    "statusCode": "200",
+                    "body": "{\"message\":\"ok\"}"
+                });
+            }).catch((err) => {
+                callback(null, {
+                    "statusCode": "500",
+                    "body": err
+                });
+            })
         })
 
     }
@@ -73,7 +94,7 @@ exports.handler = (event, context, callback) => {
                     "body": err.message
                 });
             }
-            else     {       
+            else     {console.log(data);           // successful response
             callback(null, {
                 "statusCode": "200",
                 "headers": {
